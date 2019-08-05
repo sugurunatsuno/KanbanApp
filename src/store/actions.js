16029155f1fdbb0b944abc2970e5ac6baf942a15
sugurunatsuno <suguru.irie@gmail.com>
2019-08-05 11:47:27 +0900

    アクションの雛形を作成

---
+++ src/store/actions.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import {Auth, List, Task} from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+    login: ({commit}) => {
+        // todo:
+        throw new Error('login action should be implemented')
+    },
+
+    fetchList: ({commit}) => {
+        // todo:
+        throw new Error('fetchList action should be implemented')
+    },
+
+    addTask: ({commit}) => {
+        // todo:
+        throw new Error('addTask action should be implemented')
+    },
+
+    updateTask: ({commit}) => {
+        // todo:
+        throw new Error('updateTask action should be implemented')
+    },
+
+    removeTask: ({commit}) => {
+        // todo:
+        throw new Error('removeTask action should be implemented')
+    },
+
+    logout: ({commit}) => {
+        // todo:
+        throw new Error('logout action should be implemented')
+    }
+}
